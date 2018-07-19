567167d990e345e0adf00628155bd49569178cc3
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-07-18 21:18:33 -0400

    deleted polls app, refactored base templates, added author and post models
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.post_list, name='posts')
 ]
--- main/views.py
+++ main/views.py
@@ -1,2 +1,12 @@
+from django.shortcuts import render
 
 
+def home(request):
+    """
+    View Function for home page of the site.
+    :param request:
+    :return:
+    """
+    return render(
+        request, 'main/home.html', context={}
+    )
