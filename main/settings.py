ad0cfc080f135b47ebbf4b0471fa3fa33fd081e2
MihaiChelaru <mihai72@gmail.com>
2018-07-21 16:24:05 -0400

    database update
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -85,12 +85,9 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
