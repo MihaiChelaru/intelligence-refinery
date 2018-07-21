afb6f2f83930a94746c4c10494070348511220f7
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-07-21 13:28:46 -0400

    added Pipfile.lock
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,5 +137,7 @@ STATICFILES_DIRS = (
 )
 
 FORCE_LOWERCASE_TAGS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
