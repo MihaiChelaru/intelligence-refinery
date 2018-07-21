44c78c46354912139b812235b2285fe5ebdcdc89
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-07-21 17:12:26 -0400

    remove ssl_require=True from database
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        dj_database_url.config(conn_max_age=600, ssl_require=True)
+        dj_database_url.config(conn_max_age=600)
     }
 }
 
