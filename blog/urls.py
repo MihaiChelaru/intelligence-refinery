2dafd75bc44de146c685f79706186e5e133091fe
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-07-23 20:40:06 -0400

    added resources page, content in homepage
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:post_id>/<slug:slug>/', views.post_detail, name='post-detail'),
     path('tags/', views.tag_list, name='tag-list'),
     path('tags/<slug:slug>/', views.posts_by_tag, name='posts-by-tag'),
+    path('resources/', views.resources, name='resources'),
 ]
