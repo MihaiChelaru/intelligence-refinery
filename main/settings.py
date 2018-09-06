b19c3d2dd01a0f4ad6942ddc898e500bb7f23871
MihaiChelaru <mihai72@gmail.com>
2018-09-06 14:01:05 -0400

    added reveal.js test template and dependencies
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')2cvw_ldp&h74fnt=%5sg-i6nb+-$*v6p7#l@6b%y6u2!^o@ot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["perennialbeginner.herokuapp.com"]
 
