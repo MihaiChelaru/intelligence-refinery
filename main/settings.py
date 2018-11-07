40a681cc2d5a76e911aa677afad8e606d70d33e8
MihaiChelaru <mihai72@gmail.com>
2018-11-07 07:34:22 -0500

    fixed debug mode
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "qfk2+_!0z84(__3&ca932f0^l@^huof%mpx&4
 DEBUG = os.environ.get('DEBUG', False)
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '.intelligencerefinery.io']
+ALLOWED_HOSTS = ['intelligencerefinery.herokuapp.com', 'www.intelligencerefinery.io']
 
 # Application definition
 
