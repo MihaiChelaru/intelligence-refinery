ad55a571a5ac3db6123c17db902024469fe3a341
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-11-06 22:22:10 -0500

    added contact form and email functionality
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', "a_9boeqr1-dx!6^a+&3ks^odyqjx*$8z=o1^gp&881@1dw4kjy")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["intelligencerefinery.herokuapp.com"]
 
