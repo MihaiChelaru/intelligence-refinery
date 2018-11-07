2067a4c1317c421e342ca56c6e0180f1e9cee32e
MihaiChelaru <mihai72@gmail.com>
2018-11-07 08:10:33 -0500

    fixed debug mode
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', '.intelligencerefinery.io']
 
 # Application definition
 
