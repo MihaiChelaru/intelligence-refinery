f4a41d147c5280602da30c0bd818da9f4e83df57
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-11-18 22:42:23 -0500

    added review-detail view
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
 app_name = 'reviews'
 
 #TODO: add reviews-by-tag view similar to blog equivalent
 urlpatterns = [
     path('', TemplateView.as_view(template_name='reviews/course_reviews.html'), name='reviews'),
-    path('<int:post_id>/<slug:slug>/', TemplateView.as_view(template_name='reviews/review_detail.html'), name='review-detail')
+    path('<int:review_id>/<slug:slug>/', views.review_detail, name='review-detail')
 ]
---
+++ reviews/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from markdownx.utils import markdownify
+
+from .models import Review
+
+
+def review_detail(request, review_id, slug):
+    """
+    View function for viewing an individual blog post. Redirects to the correct slug if it is improperly entered.
+    :param request:
+    :param post_id:
+    :param slug:
+    :return:
+    """
+    # Checks if the slug in the URL matches the database, and if not redirects to the correct slug URL
+    review = get_object_or_404(Review, pk=review_id)
+    if review.slug != slug:
+        return redirect('reviews:review-detail', post_id=review.pk, slug=review.slug)
+    author = review.review_author
+    # markdownify() content and display on page
+    review.content = markdownify(review.content)
+    return render(request, 'blog/post_detail.html', {'post': review, 'author': author})
