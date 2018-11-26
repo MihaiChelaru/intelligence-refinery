0bde89682b89936e2f90cc973e2074f038bdc92a
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-11-26 12:13:26 -0500

    separated reviews into separate templates for courses, books, and software, added reviews mainpage and altered navbar to reflect this change
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,9 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='reviews/course_reviews.html'), name='reviews'),
+    path('', TemplateView.as_view(template_name='reviews/review_home.html'), name='reviews-home'),
+    path('courses/', TemplateView.as_view(template_name='reviews/course_reviews.html'), name='courses'),
+    path('books/', TemplateView.as_view(template_name='reviews/book_reviews.html'), name='books'),
+    path('software/', TemplateView.as_view(template_name='reviews/software_reviews.html'), name='software'),
     path('<int:review_id>/<slug:slug>/', views.review_detail, name='review-detail'),
 ]
