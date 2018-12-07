60da7307b9e50d7b7b6617eba564cccc6507f0f3
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-12-07 09:54:03 -0500

    removed obsolete review templates and urls
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,9 +7,6 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='reviews/review_home.html'), name='reviews-home'),
-    # path('courses/', views.course_list, name='courses'),
-    # path('books/', views.book_list, name='books'),
-    # path('software/', views.software_list, name='software'),
     path('<slug:resource_type>/', views.review_list, name='review-list'),
     path('<int:review_id>/<slug:slug>/', views.review_detail, name='review-detail'),
 ]
