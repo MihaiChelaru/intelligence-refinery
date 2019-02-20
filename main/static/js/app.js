59294f0ae66d915a1ad385829df7cac630a391a3
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2019-02-19 22:22:10 -0500

    added $(document).ready to jQuery function to make it work
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/static/js/app.js
+++ main/static/js/app.js
@@ -1,8 +1,11 @@
-$(window).on('scroll', function (event) {
-    var scrollValue = $(window).scrollTop();
-    if (scrollValue > 120) {
-        $('#scrollSpy').addClass('affix');
-    } else{
-        $('#scrollSpy').removeClass('affix');
-    }
+$( document ).ready(function() {
+    // Add affix top to element when scrolling up, otherwise remove it.
+    $(window).on('scroll', function (event) {
+        var scrollValue = $(window).scrollTop();
+        if (scrollValue > 120) {
+            $('#scrollSpy').addClass('affix');
+        } else{
+            $('#scrollSpy').removeClass('affix');
+        }
+    });
 });
