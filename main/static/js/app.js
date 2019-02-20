5a4a8804502d670872f4f8d54fce1806b762c6dc
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2019-02-19 23:49:58 -0500

    formatting to use bootstrap grid
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/static/js/app.js
+++ main/static/js/app.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     // Add affix top to element when scrolling up, otherwise remove it.
     $(window).on('scroll', function (event) {
         var scrollValue = $(window).scrollTop();
-        if (scrollValue > 200) {
+        if (scrollValue > 70) {
             $('.toc').addClass('affix');
         } else{
             $('.toc').removeClass('affix');
