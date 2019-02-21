90a64d86672f37f4a6f14fae2159a996f684b431
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2019-02-20 21:41:24 -0500

    various CSS changes to make the site conform to our vision
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- main/static/js/app.js
+++ main/static/js/app.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-    // Add affix top to element when scrolling up, otherwise remove it.
-    $(window).on('scroll', function (event) {
-        var scrollValue = $(window).scrollTop();
-        if (scrollValue > 70) {
-            $('#scrollSpy').addClass('affix');
-        } else{
-            $('#scrollSpy').removeClass('affix');
-        }
-    });
-});
+// $( document ).ready(function() {
+//     // Add affix top to element when scrolling up, otherwise remove it.
+//     $(window).on('scroll', function (event) {
+//         var scrollValue = $(window).scrollTop();
+//         if (scrollValue > 70) {
+//             $('#scrollSpy').addClass('affix');
+//         } else{
+//             $('#scrollSpy').removeClass('affix');
+//         }
+//     });
+// });
